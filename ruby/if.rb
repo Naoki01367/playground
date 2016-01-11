9237388c6368af03965365b2963be29a784eeab4
杉山 尚樹 <naokisugiyama@MacBook-Air.local>
2016-01-11 18:52:13 +0900

    cteate repeat.rb

--- ruby/if.rb
+++ ruby/if.rb
@@ -12,3 +12,19 @@ if a <= 9 then
   print "smaller\n"
 end
 #thenは省略可
+
+=begin
+条件が一致したときとしなかった時で処理を変えたい場合
+if 条件 then
+  条件が成り立った時に実行したい処理
+else
+  条件が成り立たなかった時に実行したい処理
+end
+=end
+
+if a >= 10 then
+  print "bigger\n"
+else
+  print "smaller\n"
+end
+
---
+++ ruby/repeat.rb
@@ -0,0 +1,15 @@
+=begin
+繰り返しの構文
+while文
+while 繰り返し続ける条件 do
+  繰り返したい処理
+end
+=end
+
+#1から10までの数を順番に表示
+i = 1
+while i <= 10
+  print i, "\n"
+  i = i + 1
+end
+#doは省略可
